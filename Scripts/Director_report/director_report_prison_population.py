237ad85d70110af231fb16e5c720ffb4abea63de
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2020-11-27 13:33:24 +0000

    Director's report additions
    Amended copies of the male and female prison population scripts to output png charts for the quarterly director's report.

---
+++ Scripts/Director_report/director_report_prison_population.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 17:41:55 2020
+
+@author: Alex
+"""
+
+##Importing libaries
+import os
+import plotly.graph_objs as go #Offline plotting
+import plotly.io as pio
+import pandas as pd
+import datetime
+import calendar
+# import modin.pandas as pd #This utilises all CPU cores, rather than Pandas default one core 
+pio.renderers.default = "browser"
+pio.templates
+
+
+##Adding plot.ly credentials
+import chart_studio
+chart_studio.tools.set_credentials_file(username='alexhewson',
+                                  api_key='3EEPd2mvBSQd1vGCq14s')
+
+##Set working directory
+os.chdir("/Users/Alex/Prison Reform Trust/Policy and Comms Team - Documents/Python/Prison-population")
+
+
+# ##Setting templates
+prt_template = go.layout.Template(
+    layout=go.Layout(title_font=dict(
+            family="Helvetica Neue, Arial", 
+            size=17),
+    font_color = '#54565B',
+    font_family = "Helvetica Neue, Arial",
+    font_size = 12,
+    paper_bgcolor = "#FBFAF7",
+    plot_bgcolor = "#FBFAF7",
+    colorway = ("#A01D28", "#499CC9", "#F9A237", "#6FBA3A")
+    )
+)
+
+##Reading in data
+df = pd.read_csv("Data/prison_population.csv",
+                 usecols=["date", "population"],
+                 index_col=["date"],
+                 parse_dates = ['date'])
+
+df['year'] = df.index.year
+df['week'] = df.index.isocalendar().week
+df['month'] = df.index.month
+
+df['month'] = df['month'].apply(lambda x: calendar.month_abbr[x])
+
+weeks = 52/12.0
+months = [datetime.date(2020, m, 1).strftime('%b') for m in range(1, 13)]
+
+##Plotting
+
+fig = go.Figure()
+
+for year in df['year']['2017':'2020'].unique():
+  df_year = df[df['year'] == year]
+  
+  fig.add_trace(go.Scatter(x=df_year['week'], y=df_year['population'],
+                    mode='lines',
+                    connectgaps = True,
+                    hovertext =df['month'],
+                    hovertemplate = 
+                    '<b>%{hovertext}</b><br>'+
+                    '%{y:,.0f}',
+                    name=str(year)))
+
+
+##Edit the layout
+
+fig.update_layout(title="<b>Prison population in England and Wales</b>",
+                  yaxis_title='People in prison',
+                  yaxis_tickformat=',.0f',
+                  xaxis_showgrid=False,
+                  xaxis_tickmode = 'array',
+                  xaxis_tickvals=[(2*k-1)*weeks/2 for k in range(1,13)],
+                  xaxis_ticktext=months,
+                  xaxis_ticks="inside",
+                  xaxis_tickcolor='#54565B',
+                  template=prt_template,                   
+                  showlegend=True,
+                  legend=dict(
+                      yanchor="top",
+                      y=0.915),
+                  hovermode='x',
+                  modebar_activecolor="#A12833",
+                  width=655,
+                  height=500,
+                  annotations=[
+                      go.layout.Annotation(
+                          x=-0.08,
+                          y=-0.19,
+                          showarrow=False,
+                          text="<b>Source: Ministry of Justice Prison Population Bulletin\n</b>",
+                          font_size=12,
+                          xref="paper",
+                          yref="paper"
+                          )
+                      ],
+                  )
+
+fig.update_yaxes(range=[78000, 87000], nticks=6)
+
+
+'''
+This section outputs the final chart
+'''
+
+##Plot static image
+fig.write_image("images/director_report/prison_population.png", width=655, height=500, scale=2)
+
+
