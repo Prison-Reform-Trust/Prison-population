1327b753e378d54ac004e104c1c8460de8f99c0c
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2021-02-12 09:13:27 +0000

    Create run_prison_population.sh
    Creating .sh to run weekly scripts

---
+++ Scripts/run_prison_population.sh
@@ -0,0 +1,6 @@
+# USAGE: bash run_prison_population.sh
+# To produce weekly prison population Plotly visualisations and images
+
+python prison_population.py
+python female_population.py
+python HDC_caseload.py
