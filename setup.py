e24fbf553f05cba01d4b5aa97d8f68a0a4609cff
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2023-10-07 17:07:24 +0100

    New working directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='WPrison population statistics for England and Wales',
+    author='Alex Hewson',
+    license='MIT',
+)
