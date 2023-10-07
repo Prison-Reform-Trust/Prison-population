55a211e61b92756a906761b9fb1910caf1f447a9
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2023-10-07 17:03:38 +0100

    Removal of old working dir structure

--- Scripts/run_prison_population.py
+++
@@ -1,9 +0,0 @@
-import prison_population
-import female_population
-import HDC_caseload
-
-prison_population
-female_population
-HDC_caseload
-
-#Highest prison population 88,167 end of November 2011
