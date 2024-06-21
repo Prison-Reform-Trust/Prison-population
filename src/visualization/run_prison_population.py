8de9a72304f4ac78b4a15b9054b5595b0243b01d
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2024-06-21 13:59:56 +0100

    added main() function

--- src/visualization/run_prison_population.py
+++ src/visualization/run_prison_population.py
@@ -2,8 +2,12 @@ import prison_population
 import female_population
 import HDC_caseload
 
-prison_population
-female_population
-HDC_caseload
+def main():
+    prison_population
+    female_population
+    HDC_caseload
+
+if __name__ == "__main__":
+    main()
 
 #Highest prison population 88,225 13 October 2023
