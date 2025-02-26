223e15ee13310feaf48e2f3c304891f260f0e9bc
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2025-02-26 10:42:04 +0000

    WIP development

--- src/visualization/prison_population.py
+++ src/visualization/prison_population.py
@@ -1,193 +1,186 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-##Imports 
-
-#Libraries
+## Imports
 import os
 import plotly.graph_objs as go  # Offline plotting
 import chart_studio.plotly as py  # Online plotting
-import chart_studio
+import chart_studio.tools
 import plotly.io as pio
 import pandas as pd
 import datetime
 import textwrap
 from dotenv import load_dotenv, find_dotenv
 
-#Local scripts
+# Local scripts
 import src.data.utilities as utils
 import src.visualization.prt_theme as prt_theme
 
-##Loading environment variables and config
+## Load environment variables and config
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 config = utils.read_config()
 
-##Adding plotly credentials
+## Set Plotly credentials
 chart_studio.tools.set_credentials_file(
     username=os.getenv("PLOTLY_USERNAME"), api_key=os.getenv("PLOTLY_API_KEY")
 )
 
-##Setting template
+## Set template
 pio.templates.default = "prt_template"
 plotly_config = config['plotly']['config']
 
-##Reading in data
-df = pd.read_csv(
-    f"{config['data']['clnFilePath']}prison_population.csv",
-    usecols=["date", "population"],
-    parse_dates=["date"],
-)
+## Read data
+def load_data(filepath):
+    """Loads processed data from CSV."""
+    return pd.read_csv(filepath, parse_dates=["date"])
 
-##Filtering year range
-df_include = df.query('date.dt.year >= 2020 & date.dt.year <= 2024')
+## Filter dataset
+def filter_data(df):
+    """Filters dataset based on predefined conditions."""
+    df_filtered = df[
+        (df["group"] == "total") &
+        (df["type"] == "prison") &
+        (df["date"].dt.year >= 2021)
+    ].copy() # Filter for total prison population since 2021
+    return df_filtered
 
-##Calculating xaxis_tickvals
-start = datetime.datetime(2018, 1, 1)
-end = datetime.datetime(2018, 12, 31)
+## Calculate week numbers & month ticks
+def calculate_week_and_ticks(df):
+    """Calculates relative week numbers and month tick positions."""
+    oldest_year = df["date"].dt.year.min()
+    df["week"] = ((df["date"] - pd.Timestamp(f"{oldest_year}-01-01")).dt.days // 7) + 1
+    df["month"] = df["date"].dt.month
 
-xtick_vals = pd.date_range(start, end)
-filt = xtick_vals.is_month_start
+    # First week number of each month
+    month_weeks = df.groupby("month")["week"].first().tolist()
+    month_labels = df["date"].dt.strftime("%b").unique().tolist()
 
-month_weeks = xtick_vals[filt].isocalendar().week
+    return df, month_weeks, month_labels
 
-## Chart title
-title = textwrap.wrap("<b>Prison population in England and Wales</b>", width=65)
+## Generate traces for Plotly
+def generate_traces(df):
+    """Generates Plotly traces for each year in dataset."""
+    traces = [
+        go.Scatter(
+            x=df_year["week"],
+            y=df_year["value"],
+            mode="lines",
+            connectgaps=True,
+            hovertext=df_year["date"].dt.strftime("%d %b"),
+            hovertemplate="<b>%{hovertext}</b><br>%{y:,.0f}",
+            name=str(year),
+        )
+        for year, df_year in df.groupby(df["date"].dt.year)
+    ]
+    return traces
 
-##Plotting
-
-fig = go.Figure()
-
-trace_list = []
-for year in df_include["date"].dt.year.unique():
-    df_year = df_include[df_include["date"].dt.year == year]
-
-    trace = go.Scatter(
-        x=df_year["date"].dt.isocalendar().week,
-        y=df_year["population"],
-        mode="lines",
-        connectgaps=True,
-        hovertext=df_year["date"].dt.strftime("%d %b"),
-        hovertemplate="<b>%{hovertext}</b><br>" + "%{y:,.0f}",
-        name=str(year),
-    )
-
-    trace_list.append(trace)
-
-fig.add_traces(trace_list)
-
-
-##Edit the layout
-
-fig.update_layout(
-    margin=dict(l=64, b=75, r=64, pad=10),
-    title="<br>".join(title),
-    yaxis_dtick=2000,
-    xaxis_tickvals=month_weeks,
-    xaxis_ticktext=xtick_vals[filt].strftime("%b"),
-    hovermode='x'
-)
-
-## Chart annotations
-annotations = []
-
-y_list = [0, 0, 0, 0, 0]
-
-# Adding trace annotations
-for i in range(0, len(trace_list)):
-    if i < 4:
-        # For the first four traces, use a fixed x position
-        x_position = 52
-    else:
-        # For the current year's trace, use the last x value position
-        x_position = trace_list[i].x[-1]
-
-    annotations.append(
+## Generate annotations dynamically
+def generate_annotations(traces):
+    """Generates trace labels and source annotation."""
+    annotations = [
         dict(
             xref="x",
             yref="y",
-            x=x_position,
-            y=trace_list[i].y[-1] + y_list[i],
-            text=str(trace_list[i].name),
+            x=53 if i < 4 else trace.x[-1],  # First 4 use fixed x, others use last x position
+            y=trace.y[-1],
+            text=trace.name,
             xanchor="left",
             align="left",
             showarrow=False,
             font_color=fig.layout.template.layout.colorway[i],
             font_size=10,
         )
+        for i, trace in enumerate(traces)
+    ]
+
+    # Add source label
+    annotations.append(
+        dict(
+            xref="paper",
+            yref="paper",
+            x=-0.08,
+            y=-0.19,
+            align="left",
+            showarrow=False,
+            text="<b>Source: Ministry of Justice Prison Population Bulletin</b>",
+            font_size=12,
+        )
     )
 
-# Adding source label
-annotations.append(
-    dict(
-        xref="paper",
-        yref="paper",
-        x=-0.08,
-        y=-0.19,
-        align="left",
-        showarrow=False,
-        text="<b>Source: Ministry of Justice Prison Population Bulletin</b>",
-        font_size=12,
+    # Add y-axis label
+    annotations.append(
+        dict(
+            xref="x",
+            yref="paper",
+            x=1,
+            y=1.04,
+            align="left",
+            xanchor="left",
+            showarrow=False,
+            text="People in prison",
+            font_size=12,
+        )
     )
-)
 
-# Adding y-axis label
-annotations.append(
-    dict(
-        xref="x",
-        yref="paper",
-        x=1,
-        y=1.04,
-        align="left",
-        xanchor="left",
-        showarrow=False,
-        text="People in prison",
-        font_size=12,
+    return annotations
+
+## Main function to create chart
+def create_chart(df, xaxis_tickvals, xaxis_ticktext, traces):
+    """Creates the Plotly chart with all elements."""
+    fig = go.Figure(traces)
+
+    title = textwrap.wrap("<b>Prison population in England and Wales</b>", width=65)
+
+    fig.update_layout(
+        margin=dict(l=64, b=75, r=64, pad=10),
+        title="<br>".join(title),
+        yaxis_dtick=2000,
+        xaxis_tickvals=xaxis_tickvals,
+        xaxis_ticktext=xaxis_ticktext,
+        hovermode='x',
+        annotations=generate_annotations(traces),
     )
-)
 
-# Adding annotations to layout
-fig.update_layout(annotations=annotations)
+    fig.update_yaxes(range=[75900, 90100], nticks=6)
+    fig.update_xaxes(range=[1, 53])
 
-fig.update_yaxes(range=[75900, 90100], nticks=6)
-fig.update_xaxes(range=[1, 52])
+    return fig
 
-##Plot file offline
-fig.show(config=plotly_config, renderer='browser')
+## Save chart (offline and online)
+def save_chart(fig):
+    """Saves the chart as an image and uploads it online."""
+    fig.write_image(os.path.join(config['viz']['outPath'], 'prison_population.svg'))
 
-"""
-Outputting image and online charts
-"""
+    fig.layout.images = [
+        dict(
+            source="https://i.ibb.co/jhfYbyc/PRTlogo-RGB.png",
+            xref="paper",
+            yref="paper",
+            x=-0.08,
+            y=1.25,
+            sizex=0.15,
+            sizey=0.15,
+            xanchor="left",
+            yanchor="top",
+        )
+    ]
 
-##Image
-# fig.write_image(os.path.join(config['viz']['outPath'], 'prison_population.eps'))
-fig.write_image(os.path.join(config['viz']['outPath'], 'prison_population.svg'))
-
-##Online
-
-# PRT logo
-fig.layout.images = [
-    dict(
-        source="https://i.ibb.co/jhfYbyc/PRTlogo-RGB.png",
-        xref="paper",
-        yref="paper",
-        x=-0.08,
-        y=1.25,
-        sizex=0.15,
-        sizey=0.15,
-        xanchor="left",
-        yanchor="top",
+    layout_atr = prt_theme.pio.templates["prt_template"].layout
+    fig.update_layout(
+        width=layout_atr.width,
+        height=layout_atr.height,
     )
-]
 
-# Restating explicit width and height values from the template to prevent responsive
-# resizing of chart on upload to Chart Studio. This only affects CS uploads.
+    py.plot(fig, filename="Weekly prison population E&W")
 
-layout_atr = prt_theme.pio.templates["prt_template"].layout
+## Run script
+if __name__ == "__main__":
+    df = load_data(f"{config['data']['clnFilePath']}processed_data.csv")
+    df_filtered = filter_data(df)
+    df_final, xaxis_tickvals, xaxis_ticktext = calculate_week_and_ticks(df_filtered)
+    traces = generate_traces(df_final)
+    fig = create_chart(df_final, xaxis_tickvals, xaxis_ticktext, traces)
 
-fig.update_layout(
-    width=layout_atr.width,
-    height=layout_atr.height,
-)
-
-py.plot(fig, filename="Weekly prison population E&W")
+    fig.show(config=plotly_config, renderer='browser')
+    save_chart(fig)
