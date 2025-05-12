2f5d8e5a00532d64f4d28a611671d3d043c6de46
Alex Hewson <69345308+alexhewson@users.noreply.github.com>
2025-05-12 16:29:24 +0100

    Refactor function parameters for clarity and improve import statements in visualization scripts

--- src/visualization/run_prison_population.py
+++ src/visualization/run_prison_population.py
@@ -1,13 +1,14 @@
-import prison_population
-import female_population
-import HDC_caseload
+from src.visualization import (HDC_caseload, female_population,
+                               prison_population)
+
 
 def main():
-    prison_population
-    female_population
-    HDC_caseload
+    prison_population.main()
+    female_population.main()
+    HDC_caseload.main()
+
 
 if __name__ == "__main__":
     main()
 
-#Highest prison population 88,521 6 September 2024
+# Highest prison population 88,521 6 September 2024
