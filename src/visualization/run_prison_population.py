c511f72c69c0b508c15cef9b3ded10698ad98b0e
Alex Hewson <69345308+alexhewson@users.noreply.github.com>
2025-06-16 13:57:28 +0100

    Refactor main function to include data downloading and processing, and chart creation for improved clarity

--- src/visualization/run_prison_population.py
+++ src/visualization/run_prison_population.py
@@ -1,11 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from src.data import download_data, make_dataset, weekly_data_summary
 from src.visualization import (HDC_caseload, female_population,
                                prison_population)
 
 
-def main():
+def download_data_and_make_dataset():
+    download_data.download_prison_population_data(years=2025)
+    make_dataset.main()
+    weekly_data_summary.main()
+
+
+def make_charts():
     prison_population.main()
     female_population.main()
     HDC_caseload.main()
+
+
+def main():
+    download_data_and_make_dataset()
+    make_charts()
 
 
 if __name__ == "__main__":
